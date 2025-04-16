aa7912fbc84c7ddc1debd1ba8db66cac98ee62db
nami-04 <namithar2004@gmail.com>
2025-04-16 18:45:15 +0530

    Update wsgi.py to load environment variables

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+# Load environment variables from .env file
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 
